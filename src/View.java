0f35e9bce5ec70cbac821dbeb7d8aa2febe92bc2
rh137 <b06902137@csie.ntu.edu.tw>
2021-06-22 18:05:44 +0800

    feat: add the button for choosing level 4

--- src/View.java
+++ src/View.java
@@ -106,6 +106,14 @@ public class View extends JFrame{
 	        }
 	    });
 	    selectionPanel.add(btn5);
+		JButton btn__ = new JButton("Level 4");
+		btn__.addActionListener(new ActionListener() {
+			@Override
+			public void actionPerformed(ActionEvent e) {
+				game.play(4);
+			}
+		});
+		selectionPanel.add(btn__);
 	    JButton btn6 = new JButton("main");
     	btn6.addActionListener(new ActionListener() {
 	        @Override
