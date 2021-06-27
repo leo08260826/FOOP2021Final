888da40c520abba68e253d72979bfc9cb5eace3e
allpass122 <dges5102@gmail.com>
2021-06-27 19:07:50 +0800

    fix: randombrick assert newVx != 0

--- src/RandomBrick.java
+++ src/RandomBrick.java
@@ -1,31 +1,30 @@
-public class RandomBrick extends Brick  {
-    public RandomBrick(String _name, int _x, int _y, int _z, int _width, int _height, String _tag)
-    {
+public class RandomBrick extends Brick {
+    public RandomBrick(String _name, int _x, int _y, int _z, int _width, int _height, String _tag) {
         super(_name, _x, _y, _z, _width, _height, _tag);
     }
+
     private int randomRange = 2;
-    private int getRandom(){
-        return ((int)(Math.random()*2*randomRange)+1)-randomRange;
+
+    private int getRandom() {
+        return ((int) (Math.random() * 2 * randomRange) + 1) - randomRange;
     }
 
     @Override
-    public void collided(GameObject other)
-    {
-    	// System.out.println("touch RandomBrick");
+    public void collided(GameObject other) {
+        // System.out.println("touch RandomBrick");
         this.setIsDead(true);
-        if(other instanceof Ball){
+        if (other instanceof Ball) {
             // [0,6] and -3 = [-3,3]
-            ((Ball)other).setVx(getRandom());
-            int vy_ = getRandom();
-            // assert vy_ != 0
-            // while( Math.abs(vy_) <= 1){
-            //     vy_ = getRandom();
-            // }
-            // ((Ball)other).setVy(vy_);
+            // assert vx_ != 0
+            int vx_ = getRandom();
+            while (Math.abs(vx_) <= 1) {
+                vx_ = getRandom();
+            }
+            ((Ball) other).setVx(vx_);
         }
     }
 
-    public String imagePath(){
+    public String imagePath() {
         return "./src/images/PNG/05-Breakout-Tiles.png";
     }
 }
